4dd247eeb5c0a38c69ab55a219506c834b5647e7
Charles L <charles.lobo@gmail.com>
2026-01-19 07:56:29 +1100

    fix: overflow display issue

--- ui/src/components/LogViewer.tsx
+++ ui/src/components/LogViewer.tsx
@@ -406,6 +406,7 @@ export function LogViewer({
                   left: 0,
                   width: '100%',
                   transform: `translateY(${virtualItem.start}px)`,
+                  overflow: 'hidden',
                 }}
               >
                 <LogLine
